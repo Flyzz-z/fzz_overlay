845b04e4fdf6fea77dc15b1d12a77284405937bd
flyzz <xiangzz2001@163.com>
2023-04-12 06:53:12 +0000

    delete .cmd

--- .overlay.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/flyzz/linux-5.3.5/fs/overlayfs.back/overlay.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/flyzz/linux-5.3.5/fs/overlayfs.back/overlay.ko /home/flyzz/linux-5.3.5/fs/overlayfs.back/overlay.o /home/flyzz/linux-5.3.5/fs/overlayfs.back/overlay.mod.o ;  true
